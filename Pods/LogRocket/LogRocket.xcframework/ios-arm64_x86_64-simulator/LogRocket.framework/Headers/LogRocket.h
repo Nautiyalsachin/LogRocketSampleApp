d971c825147ae7ce9952d1f5493ae42d6361c52f
Sachin Nautiyal <sachinnautiyal00@gmail.com>
2022-01-28 15:15:50 +0530

    feat: Updated logrocket SDK from 0.21 to 0.24.

--- Pods/LogRocket/LogRocket.xcframework/ios-arm64_x86_64-simulator/LogRocket.framework/Headers/LogRocket.h
+++ Pods/LogRocket/LogRocket.xcframework/ios-arm64_x86_64-simulator/LogRocket.framework/Headers/LogRocket.h
@@ -8,3 +8,4 @@ FOUNDATION_EXPORT const unsigned char LogRocketVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <LogRocket/PublicHeader.h>
 #import <LogRocket/LROReactNativeSwizzler.h>
+#import <LogRocket/LRORNSVGSwizzle.h>
--- Pods/Target Support Files/Pods-LogRocketSampleApp/Pods-LogRocketSampleApp-frameworks.sh
+++ Pods/Target Support Files/Pods-LogRocketSampleApp/Pods-LogRocketSampleApp-frameworks.sh
@@ -176,11 +176,9 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/SwiftProtobuf/SwiftProtobuf.framework"
   install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/LogRocket/LogRocket.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/SwiftProtobuf/SwiftProtobuf.framework"
   install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/LogRocket/LogRocket.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
